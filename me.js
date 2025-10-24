5eb9871d2976bb8c62bf10b415582bdca0325f74
𝚸𝚮𝚯𝚵𝚴𝚰𝚾 <piyushguptaofficial07042009@gmail.com>
2025-10-24 18:48:40 +0530

    Create me.js

---
+++ me.js
@@ -0,0 +1,11 @@
+{
+  "Hi": "*💖HI*... *How are You..!*",
+  "Good Morning": "*Good Morning 🌅*",
+  "Good Night": "*Good Night..🌉*",
+  "Bye": "*Subhratri radhe radhe....*",
+  "owner": "*Phoenix*",
+  "Bot": "Kis Na Yad Kia Mujha?..🐵",
+  "link": "*https://chat.whatsapp.com/EVWWjqbDCmg5TZHSJ0vm6O?mode=ems_copy_t*",
+  "hmm": "> *Hmmmm.*",
+  "love": "*Lub you too 💗😁*"
+}
